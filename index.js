5d74f41d9068c47e8e2751e5c8f492ec640fe2dc
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-11-18 14:55:12 +0500

    added prompt functionality

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+(async() => {
+
+    try {
+
+      const replace = await prompt("Enter what you want to replace: ");
+      const replaceWith = await prompt("Enter what you want it to be replaced with: ");
+      const preview = await prompt("Do you want to preview before renaming? (Y/N): ");
+
+      const folder = "data";
+
+      try{
+        fs.readdir(folder, (data)=>{
+            console.log(data);
+        });
+      }catch{
+
+      }
+
+      rl.close();
+
+    } catch (e) {
+
+      console.error(e);
+
+    }
+
+  })();
